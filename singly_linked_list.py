2c07c3dcb80d2a3c71b90898b4fef1a6f3a891a0
dklevakov <dklevakov@gmail.com>
2021-03-28 17:18:20 +0300

    SinglyLinkedList

---
+++ singly_linked_list.py
@@ -0,0 +1,12 @@
+class LinkedList:
+    head = None
+    length = 0
+
+    class Node:
+        element = None
+        next_node = None
+
+        def __init__(self, element, next_node=None):
+            self.element = element
+            self.next_node = next_node
+
