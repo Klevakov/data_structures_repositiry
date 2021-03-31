ce95e62296fe0d6e455105d2472c2ee61e5396fd
dklevakov <dklevakov@gmail.com>
2021-03-31 16:47:29 +0300

    Added annotation, __str__ method. Now the .insert() and .pop() methods work by calling the .get() method.

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -1,3 +1,6 @@
+from typing import Any, Union
+
+
 class LinkedList:
     head = None
     tail = None
@@ -5,11 +8,14 @@ class LinkedList:
 
     class Node:
 
-        def __init__(self, value, next_node=None) -> None:
+        def __init__(self, value: Any, next_node=None) -> None:
             self.value = value
             self.next_node = next_node
 
-    def append(self, value):
+        def __str__(self):
+            return f'The Node with value {self.value}'
+
+    def append(self, value: Any) -> None:
         """Adding a node to the end of the list."""
         new_node = self.Node(value)
         if not self.head:
@@ -19,43 +25,39 @@ class LinkedList:
             self.tail.next_node = new_node
             self.tail = new_node
         self.length += 1
+        print(f'{new_node} was added')
 
     def out(self) -> None:
         """Print all values of the nodes of the list"""
         if not self.head:
-            return print('The list is empty')
-        node = self.head
-        while node.next_node:
-            print(node.value, end=' / ')
-            node = node.next_node
-        print(node.value)
+            print('The list is empty')
+        else:
+            node = self.head
+            while node.next_node:
+                print(node.value, end=' / ')
+                node = node.next_node
+            print(node.value)
 
-    def insert(self, value, index: int):
+    def insert(self, value: Any, index: int) -> None:
         """Adding a node to the list at a specific place in the list by index."""
         node = self.head
-
         if index == 0:
             new_node = self.Node(value, next_node=node)
             self.head = new_node
             self.length += 1
             if self.length == 1:
                 self.tail == new_node
-            print(f'The node with the value "{value}" is added to the list at position "{index}"')
+            print(f'{new_node}" was inserted to the list at position "{index}"')
         else:
-            i = 0
-            while i < index:
-                previous_node = node
-                if i == index - 1 and index == self.length:
-                    node.next_node = self.Node(value)
-                    self.length += 1
-                    return print(f'The node with the value "{value}" is added to the list at position "{index}"')
-                node = node.next_node
-                i += 1
-            previous_node.next_node = self.Node(value, next_node=node)
+            previous_node = self.get(index - 1)
+            new_node = self.Node(value, next_node=previous_node.next_node)
+            previous_node.next_node = new_node
+            if index == self.length:
+                self.tail = new_node
             self.length += 1
-            print(f'The node with the value "{value}" is added to the list at position "{index}"')
+            print(f'The node with the value "{value}" was inserted to the list at position "{index}"')
 
-    def get(self, index: int) -> str:
+    def get(self, index: int) -> Node:
         """Get information about a node by its ordinal number in the list."""
         i = 0
         node = self.head
@@ -64,69 +66,63 @@ class LinkedList:
             node = node.next_node
             i += 1
         if node == self.head:
-            return f'The node with the value "{node.value}" is the head and has a link to the next node with the value "{node.next_node.value}"'
+            print(f'The node with the value "{node.value}" is the head and has a link to the next node with the value "{node.next_node.value}"')
         elif node.next_node:
-            return f'The node with the value "{node.value}" has a link to the next node with the value "{node.next_node.value}"'
+            print(f'The node with the value "{node.value}" has a link to the next node with the value "{node.next_node.value}"')
         else:
-            return f'The node with the value "{node.value}" is the last one'
+            print(f'The node with the value "{node.value}" is the last one')
+        return node
 
-    def delete(self, index: int):
+    def pop(self, index: int) -> Union[Node, None]:
         """Deleting a node by its ordinal number in the list."""
         if self.length == 0:
-            return print('Deletion is not possible. List is empty.')
-            # raise IndexError('Deletion is not possible. List is empty.')
+            print('Deletion is not possible. List is empty.')
+            return None
         if index >= self.length:
-            return print(f'Deletion is not possible. The list does not contain the {index} item')
-            # raise IndexError(f'Deletion is not possible. The list does not contain the {index} item')
+            print(f'Deletion is not possible. The list does not contain the {index} item')
+            return None
         if self.length == 1:
-            remove_value = self.head.value
+            remove_node = self.head
             self.tail = self.head = None
             self.length -= 1
-            return print(f'The node with the value "{remove_value}" was deleted')
+            print(f'{remove_node} was deleted.Now the list is empty')
+            return remove_node
         if index == 0:
+            remove_node = self.head
             self.head = self.head.next_node
             self.length -= 1
+            print(f'{remove_node} was deleted.')
+            return remove_node
         else:
-            i = 0
-            node = self.head
+            previous_node = self.get(index - 1)
+            remove_node = previous_node.next_node
+            previous_node.next_node = remove_node.next_node
             if index == self.length - 1:
-                while i < index:
-                    previous_node = node
-                    node = node.next_node
-                    i += 1
-                remove_elem = node.value
-                print(f'удаляем {remove_elem}')
-                previous_node.next_node = None
-                # НУжно ли тут прописать del node? для очистки памяти, ведь экземпляр класса создан, просто на него больше нет ссылок
                 self.tail = previous_node
-                self.length -= 1
-                return print(f'The node with the value "{remove_elem}" was deleted')
-            while i <= index:
-                previous_node = node
-                node = node.next_node
-                i += 1
-            remove_elem = previous_node.value
-
-            previous_node.value = node.value
-            previous_node.next_node = node.next_node
             self.length -= 1
-            print(f'The node with the value "{remove_elem}" was deleted')
+            print(f'{remove_node} was deleted')
+            return remove_node
 
-    def get_len(self) -> None:
+    def get_len(self) -> int:
         print(f'List length = " {self.length}"')
+        return self.length
+
+
+
 
 
 linked_list = LinkedList()
 
 linked_list.out()
 linked_list.append(10)
+linked_list.insert(80, 0)
 linked_list.append(20)
 linked_list.append(30)
 linked_list.append(40)
 linked_list.append(50)
 linked_list.insert(100, 0)
 linked_list.out()
-linked_list.insert(100, 6)
+linked_list.insert(100, 7)
 linked_list.out()
 
 
