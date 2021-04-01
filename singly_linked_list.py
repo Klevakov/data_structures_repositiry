b307acc472507ffc733f31237a828fb3adba96ad
dklevakov <dklevakov@gmail.com>
2021-04-01 11:00:31 +0300

    Added tests

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -25,21 +25,27 @@ class LinkedList:
             self.tail.next_node = new_node
             self.tail = new_node
         self.length += 1
-        print(f'{new_node} was added')
+        # print(f'{new_node} was added')
 
-    def out(self) -> None:
+    def out(self) -> str:
         """Print all values of the nodes of the list"""
         if not self.head:
-            print('The list is empty')
+            return 'The list is empty'
+            # print('The list is empty')
         else:
             node = self.head
+            res_str = ''
             while node.next_node:
-                print(node.value, end=' / ')
+                res_str += f'{node.value} '
+                # print(node.value, end=' / ')
                 node = node.next_node
-            print(node.value)
+            # print(node.value)
+            return res_str + f'{node.value} '
 
     def insert(self, value: Any, index: int) -> None:
         """Adding a node to the list at a specific place in the list by index."""
+        if index < 0 or index >= self.length and index != 0:
+            raise IndexError
         node = self.head
         if index == 0:
             new_node = self.Node(value, next_node=node)
@@ -47,7 +53,7 @@ class LinkedList:
             self.length += 1
             if self.length == 1:
                 self.tail == new_node
-            print(f'{new_node}" was inserted to the list at position "{index}"')
+            # print(f'{new_node}" was inserted to the list at position "{index}"')
         else:
             previous_node = self.get(index - 1)
             new_node = self.Node(value, next_node=previous_node.next_node)
@@ -55,86 +61,76 @@ class LinkedList:
             if index == self.length:
                 self.tail = new_node
             self.length += 1
-            print(f'The node with the value "{value}" was inserted to the list at position "{index}"')
+            # print(f'The node with the value "{value}" was inserted to the list at position "{index}"')
 
     def get(self, index: int) -> Node:
         """Get information about a node by its ordinal number in the list."""
+        if self.length == 0 or 0 > index > self.length - 1:
+            raise IndexError
         i = 0
         node = self.head
         while i < index:
             previous_node = node
             node = node.next_node
             i += 1
-        if node == self.head:
-            print(f'The node with the value "{node.value}" is the head and has a link to the next node with the value "{node.next_node.value}"')
-        elif node.next_node:
-            print(f'The node with the value "{node.value}" has a link to the next node with the value "{node.next_node.value}"')
-        else:
-            print(f'The node with the value "{node.value}" is the last one')
+        # if node == self.head:
+            # print(f'The node with the value "{node.value}" is the head and has a link to the next node with the value "{node.next_node.value}"')
+        # elif node.next_node:
+            # print(f'The node with the value "{node.value}" has a link to the next node with the value "{node.next_node.value}"')
+        # else:
+            # print(f'The node with the value "{node.value}" is the last one')
         return node
 
-    def pop(self, index: int) -> Union[Node, None]:
+    def pop(self, index: int) -> Any:
         """Deleting a node by its ordinal number in the list."""
         if self.length == 0:
-            print('Deletion is not possible. List is empty.')
-            return None
+            raise IndexError
+            # print('Deletion is not possible. List is empty.')
+            # return None
         if index >= self.length:
-            print(f'Deletion is not possible. The list does not contain the {index} item')
-            return None
+            raise IndexError
+            # print(f'Deletion is not possible. The list does not contain the {index} item')
+            # return None
         if self.length == 1:
-            remove_node = self.head
+            remove_value = self.head.value
             self.tail = self.head = None
             self.length -= 1
-            print(f'{remove_node} was deleted.Now the list is empty')
-            return remove_node
+            # print(f'{remove_node} was deleted.Now the list is empty')
+            return remove_value
         if index == 0:
-            remove_node = self.head
+            remove_value = self.head.value
             self.head = self.head.next_node
             self.length -= 1
-            print(f'{remove_node} was deleted.')
-            return remove_node
+            # print(f'{remove_node} was deleted.')
+            return remove_value
         else:
             previous_node = self.get(index - 1)
-            remove_node = previous_node.next_node
+            remove_value = previous_node.next_node.value
             previous_node.next_node = remove_node.next_node
             if index == self.length - 1:
                 self.tail = previous_node
             self.length -= 1
-            print(f'{remove_node} was deleted')
-            return remove_node
+            # print(f'{remove_node} was deleted')
+            return remove_value
 
     def get_len(self) -> int:
-        print(f'List length = " {self.length}"')
+        # print(f'List length = " {self.length}"')
         return self.length
 
-
-
-
-
-linked_list = LinkedList()
-
-linked_list.out()
-linked_list.append(10)
-linked_list.insert(80, 0)
-linked_list.append(20)
-linked_list.append(30)
-linked_list.append(40)
-linked_list.append(50)
-linked_list.insert(100, 0)
-linked_list.out()
-linked_list.insert(100, 7)
-linked_list.out()
-
-
-# linked_list.get(2)
-# linked_list.insert(80, 0)
-# linked_list.out()
-# linked_list.insert(800, 2)
-# linked_list.out()
-# linked_list.delete(0)
-# linked_list.out()
-# linked_list.delete(3)
-# linked_list.out()
-# print(linked_list.length)
-# linked_list.delete(linked_list.length - 1)
-# linked_list.out()
+    def assign(self, value: Any, index: int) -> Any:
+        """The method replaces the node's value and returns the old value."""
+        if index < 0 or self.length == 0 or index > self.length - 1:
+            raise IndexError
+        if index == 0:
+            remove_value = self.head.value
+            self.head.value = value
+            if self.length == 1:
+                self.tail.value == value
+            return remove_value
+        else:
+            node = self.get(index)
+            remove_value = node.value
+            node.value = value
+            if index == self.length - 1:
+                self.tail = node
+            return remove_value
